b075f96f2909f018486bad427152e5760f3e8219
creeds-knight <apedoarthur21@gmail.com>
2024-10-08 12:32:50 +0300

    adding file route

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@
  */
 import AppController from "../controllers/AppController";
 import UsersController from "../controllers/UsersController"
+import AuthController from "../controllers/AuthController";
+import FilesController from "../controllers/FilesController";
+
 const routing = (app) => {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
+  app.get('/connect', AuthController.getConnect);
+  app.get('/disconnect', AuthController.getDisconnect);
+  app.get('/users/me', UsersController.getMe);
+  app.get('/files', FilesController.postUpload);
+
 }
 
 export default routing;
