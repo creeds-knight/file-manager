4b4da27795515e708df65aaf5f3bb770512f3f47
creeds-knight <apedoarthur21@gmail.com>
2024-11-15 23:30:28 +0300

    adding swagger documentation

--- server.js
+++ server.js
@@ -3,13 +3,15 @@
  */
 import express from 'express';
 import routing from './routes/index';
+import setupSwagger from './swagger';
 
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express(); // Entry point into the app
+const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
+setupSwagger(app); // Settinh up swagger documentation
 routing(app);
 
 app.listen(port, '0.0.0.0', () => {
